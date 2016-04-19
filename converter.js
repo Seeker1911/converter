aa6043b0639fdedd6e8b663eeb7fb9109ba5f79b
Michael Mead <michaelmead007@icloud.com>
2016-04-19 11:14:07 -0500

    finish colors, add keypress event

--- converter.js
+++ converter.js
@@ -14,38 +14,57 @@
 
 var fahrenheit = "";
 var celsius = "";
+var answer = document.getElementById("answer");
 
 function toCelsius() {
     celsius = (temp.value - 32) * 5 / 9;
 }
 
 function toFahrenheit() {
-   fahrenheit = (temp.value * 9) / 5 + 32;
+    fahrenheit = (temp.value * 9) / 5 + 32;
 }
 
 // Get a reference to the button element in the DOM
 var button = document.getElementById("converter");
 var temp = document.getElementById("temp");
-// fahrenheit = temp.value;
-// celsius = temp.value;
 
 // This function should determine which conversion should
 // happen based on which radio button is selected.
 function determineConverter(click) {
-    console.log("event", click);
+    // console.log("event", click);
     if (document.getElementById("cel").checked === true) {
         toCelsius();
         document.getElementById("answer").innerHTML = celsius;
+        if (celsius >= 32) {
+            answer.style.color = "#ff0000"
+        } else if (celsius <= 0) {
+            answer.style.color = "#0000ff"
+        } else {
+            answer.style.color = "#00ff00"
+        }
     } else if (document.getElementById("far").checked === true) {
-    	toFahrenheit();
+        toFahrenheit();
         document.getElementById("answer").innerHTML = fahrenheit;
+        if (fahrenheit >= 90) {
+            answer.style.color = "#ff0000"
+        } else if (fahrenheit <= 32) {
+            answer.style.color = "#0000ff"
+        } else {
+            answer.style.color = "#00ff00"
+        }
     }
 };
 
 // Assign a function to be executed when the button is clicked
 button.addEventListener("click", determineConverter);
-document.addEventListener("return", determineConverter);
-document.getElementById("clear").addEventListener("click", function(){
-	document.getElementById("temp").value = "";
-	document.getElementById("answer").innerHTML = "";
+// Clear input and answer on click.
+document.getElementById("clear").addEventListener("click", function() {
+    document.getElementById("temp").value = "";
+    document.getElementById("answer").innerHTML = "";
 });
+temp.addEventListener("keypress", tempEnter)
+function tempEnter (keypress) {
+		if (keypress.which === 13) {
+			determineConverter();
+		}
+}
